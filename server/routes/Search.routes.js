aa7d2ec918a04eb1f7179cb0a94999dfcbf6d006
Jerico Navarro <jericoxnavarro16@gmail.com>
2020-12-04 22:24:41 +0800

    Search routes is done added shoeName and StyleId to the query checker

--- server/routes/Search.routes.js
+++ server/routes/Search.routes.js
@@ -1,21 +1,48 @@
 const express = require("express");
 const router = express.Router();
 const Brands = require("../models/Brands.model");
+const Popular = require("../models/Popular.model");
 
-router.get("/search/:query", async (req, res) => {
+router.get("/search", async (req, res) => {
   try {
-    await Brands.find(
-      {
-        "sneakers.shoeName": "Nike Air Rubber Dunk Off-White UNC",
-      },
-      (err, data) => {
-        if (err) {
-          console.log(err);
-        } else {
-          res.json(data);
+    await Brands.find({}, async (err, data) => {
+      let getObj = [];
+      const popular = await Popular.find();
+      let getArr = data[0].sneakers.concat(
+        data[1].sneakers,
+        data[2].sneakers,
+        data[3].sneakers,
+        data[4].sneakers,
+        data[5].sneakers,
+        popular[0].sneakers
+      );
+      for (let i = 0; i < getArr.length; i++) {
+        if (
+          getArr[i].shoeName
+            .toLowerCase()
+            .includes(req.query.query.toLocaleLowerCase())
+        ) {
+          getObj.push(getArr[i]);
+        } else if (
+          getArr[i].shoeName.toLowerCase() ===
+          req.query.query.toLocaleLowerCase()
+        ) {
+          getObj.push(getArr[i]);
+        } else if (
+          getArr[i].styleID
+            .toLowerCase()
+            .includes(req.query.query.toLocaleLowerCase())
+        ) {
+          getObj.push(getArr[i]);
+        } else if (
+          getArr[i].styleID.toLowerCase() ===
+          req.query.query.toLocaleLowerCase()
+        ) {
+          getObj.push(getArr[i]);
         }
       }
-    );
+      res.json(getObj);
+    });
   } catch (err) {
     res.json({ message: err });
   }
