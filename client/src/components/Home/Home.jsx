8f0d44cacd286b2c7a69668e58377feb864d986d
Jerico Navarro <jericoxnavarro16@gmail.com>
2020-12-06 22:01:54 +0800

    change some design and functions

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,82 +1,80 @@
 import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import Homehero from "./Hero";
 import "../../sass/Content.scss";
 import Shoebox from "../Shoebox";
 import Preloader from "../Preloader";
 import Footer from "../Footer";
+import Pagination from "../Paginations";
 
 const Home = ({ brand }) => {
-  const [products, setProducts] = useState([]);
   const [show, setShow] = useState([]);
   const [currentpage, setCurrentpage] = useState(1);
-
+  const [next, setNext] = useState(1);
+  const [previous, setPrevious] = useState(1);
+  const [pagelength, setPagelength] = useState(1);
+  const location = useLocation();
   useEffect(() => {
+    setShow([]);
     const getProducts = async () => {
+      const Query = () => {
+        return new URLSearchParams(location.search);
+      };
+      let query = Query();
       const response = await fetch(
-        `${process.env.REACT_APP_API_URI}/api/popular`
+        `${process.env.REACT_APP_API_URI}/api/popular?page=${query.get(
+          "page"
+        )}&limit=20`
       );
       const data = await response.json();
-      setProducts(data[0].sneakers);
+      setShow(data.data);
+      setNext(data.next);
+      setPrevious(data.previous);
+      setPagelength(data.pageLength);
+      setCurrentpage(parseInt(query.get("page")));
     };
     getProducts();
-  }, []);
+  }, [brand, currentpage, location]);
 
-  useEffect(() => {
-    const boxperPage = 60;
-    const starting = currentpage * boxperPage - boxperPage;
-    const ending = currentpage * boxperPage;
-    const inShow = [];
-    for (let i = starting; i < ending; i++) {
-      if (products[i] === undefined) {
-        break;
-      } else {
-        inShow.push(products[i]);
-      }
-    }
-    setShow(inShow);
-  }, [currentpage, products]);
-
-  const paginationLength = products.length / 60;
-  let buttons = [];
-  for (let i = 0; i < Math.ceil(paginationLength); i++) {
-    buttons.push(
-      <button
-        key={i + 1}
-        onClick={() => setCurrentpage(i + 1)}
-        className="page-btn"
-      >
-        {i + 1}
-      </button>
-    );
-  }
-
-  const renderHome = () => {
-    if (products.length === 0) {
+  const Render = () => {
+    if (show.length === 0) {
       return <Preloader brand={brand} />;
     } else {
       return (
         <>
-          <Homehero />
-          <main className="main-content">
-            <div className="container">
-              <h1 className="trending">Trending Now</h1>
-              <div className="grid-main">
-                {show.map((product, index) => (
-                  <div key={product._id} className="box">
-                    <Shoebox product={product} />
-                  </div>
-                ))}
+          <div className="grid-main">
+            {show.map((product, index) => (
+              <div key={product._id} className="box">
+                <Shoebox product={product} />
               </div>
-              <div className="pagination">{buttons}</div>
-            </div>
-            <Footer />
-          </main>
+            ))}
+          </div>
+          <div className="pagination">
+            <Pagination
+              location={location}
+              currentpage={currentpage}
+              pagelength={pagelength}
+              previous={previous}
+              next={next}
+            />
+          </div>
         </>
       );
     }
   };
 
-  return renderHome();
+  return (
+    <>
+      <Homehero />
+      <main className="main-content">
+        <div className="container">
+          <h1 className="trending">Trending Now</h1>
+          <Render />
+        </div>
+        <Footer />
+      </main>
+    </>
+  );
 };
 
 export default Home;
