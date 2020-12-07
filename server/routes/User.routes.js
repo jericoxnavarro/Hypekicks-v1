abe95faa5126b153f5ff6bd863cf592a70d5ca26
Jerico Navarro <jericoxnavarro16@gmail.com>
2020-12-07 19:20:23 +0800

    added validations

--- server/routes/User.routes.js
+++ server/routes/User.routes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const Joi = require("@hapi/joi");
 const User = require("../models/User.model");
+const bcrypt = require("bcryptjs");
 
 // Get all users
 router.get("/user", async (req, res) => {
@@ -32,21 +34,46 @@ router.get("/user/favorites/:userID", async (req, res) => {
   }
 });
 
+const validationSchema = Joi.object({
+  username: Joi.string().min(6).required(),
+  password: Joi.string().min(6).required(),
+  fullname: Joi.string().min(6).required(),
+  email: Joi.string().min(6).required().email(),
+  address: Joi.string().min(6).required(),
+});
+
 // Add user
-router.post("/user/add", async (req, res) => {
+router.post("/user/create", async (req, res) => {
+  // Request Validations
+  const { error } = validationSchema.validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  // Check if email exists
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist) return res.status(400).send("Email already exists");
+
+  // Check if username exists
+  const usernameExist = await User.findOne({ username: req.body.username });
+  if (usernameExist) return res.status(400).send("Username already exists");
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
   const user = new User({
     username: req.body.username,
-    password: req.body.password,
+    password: hashedPassword,
     fullname: req.body.fullname,
     email: req.body.email,
     address: req.body.address,
     favorites: [],
   });
+
   try {
     const saveUser = await user.save();
     res.json(saveUser);
   } catch (err) {
-    res.json({ message: err });
+    res.status(400).send(err);
   }
 });
 
