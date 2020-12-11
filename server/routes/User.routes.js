d07ceee78dd20267ce5f434a3f75b929383a6d78
Jerico Navarro <jericoxnavarro16@gmail.com>
2020-12-11 21:50:49 +0800

    added controllers to make the codes readable

--- server/routes/User.routes.js
+++ server/routes/User.routes.js
@@ -1,153 +1,29 @@
 const express = require("express");
 const router = express.Router();
-const Joi = require("@hapi/joi");
-const User = require("../models/User.model");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const verify = require("../routes/Verify.token");
+const verify = require("../controllers/Verify.token");
+
+const UserControllers = require("../controllers/User.controllers");
 
 // Get all users
-/*router.get("/user", verify, async (req, res) => {
-  try {
-    const brand = await User.find();
-    res.json(brand);
-  } catch (err) {
-    res.json({ message: err });
-  }
-});*/
+router.get("/users", UserControllers.getUsers);
 
 // Get user
-router.get("/user/:userID", verify, async (req, res) => {
-  // Check User ID if match
-  if (req.params.userID != req.user._id)
-    return res.status(401).send("User Access Denied");
-
-  try {
-    const user = await User.findById(req.params.userID);
-    res.json(user);
-  } catch (err) {
-    res.json({ message: err });
-  }
-});
+router.get("/user/:userID", verify, UserControllers.getUser);
 
 // Get user favorites
-router.get("/user/favorites/:userID", verify, async (req, res) => {
-  // Check User ID if match
-  if (req.params.userID != req.user._id)
-    return res.status(401).send("User Access Denied");
-
-  try {
-    const user = await User.findById(req.params.userID);
-    res.json(user.favorites);
-  } catch (err) {
-    res.json({ message: err });
-  }
-});
-
-const validationCreate = Joi.object({
-  username: Joi.string().min(6).required(),
-  password: Joi.string().min(6).required(),
-  fullname: Joi.string().min(6).required(),
-  email: Joi.string().min(6).required().email(),
-  address: Joi.string().min(6).required(),
-});
+router.get("/user/favorites/:userID", verify, UserControllers.getUserFavorites);
 
 // Add user
-router.post("/user/create", async (req, res) => {
-  // Request Validations
-  const { error } = validationCreate.validate(req.body);
-  if (error)
-    return res
-      .status(400)
-      .send({ message: error.details[0].message, status: "none" });
-
-  // Check if email exists
-  const emailExist = await User.findOne({ email: req.body.email });
-  if (emailExist)
-    return res
-      .status(400)
-      .send({ message: `"Email already exists`, status: "none" });
-
-  // Check if username exists
-  const usernameExist = await User.findOne({ username: req.body.username });
-  if (usernameExist)
-    return res
-      .status(400)
-      .send({ message: `"Username already exists`, status: "none" });
-
-  // Hash password
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(req.body.password, salt);
-
-  const user = new User({
-    username: req.body.username,
-    password: hashedPassword,
-    fullname: req.body.fullname,
-    email: req.body.email,
-    address: req.body.address,
-    favorites: [],
-  });
-
-  try {
-    const saveUser = await user.save();
-    res.json({ user: user._id, message: `"User Created` });
-  } catch (err) {
-    res.status(400).send({ message: err, status: "none" });
-  }
-});
+router.post("/user/create", UserControllers.createUser);
 
 // Update Favorites
-router.put("/user/updatefavorites/:userID", verify, async (req, res) => {
-  // Check User ID if match
-  if (req.params.userID != req.user._id)
-    return res.status(401).send("User Access Denied");
-
-  try {
-    const updateFavorites = await User.updateOne(
-      { _id: req.params.userID },
-      { $push: { favorites: [req.body.sneaker] } }
-    );
-    res.json(updateFavorites);
-  } catch (err) {
-    res.json({ message: err });
-  }
-});
-
-const validationLogin = Joi.object({
-  username: Joi.string().min(6).required(),
-  password: Joi.string().min(6).required(),
-});
+router.put(
+  "/user/updatefavorites/:userID",
+  verify,
+  UserControllers.updateUserFavorites
+);
 
 // User log in
-router.post("/user/login", async (req, res) => {
-  // Request Validations
-  const { error } = validationLogin.validate(req.body);
-  if (error)
-    return res
-      .status(400)
-      .send({ message: error.details[0].message, status: "none" });
-
-  // Check if username exists
-  const user = await User.findOne({ username: req.body.username });
-  if (!user)
-    return res
-      .status(400)
-      .send({ message: `"Username or Password is wrong`, status: "none" });
-  const validPass = await bcrypt.compare(req.body.password, user.password);
-  if (!validPass)
-    return res
-      .status(400)
-      .send({ message: `"Invalid Password`, status: "none" });
-
-  // Create and assign token
-  const payload = {
-    _id: user._id,
-    username: user.username,
-  };
-  const token = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: "1d",
-  });
-  res.header("auth-token", token).send({ token: token, _id: user._id });
-});
+router.post("/user/login", UserControllers.userLogin);
 
 module.exports = router;
