0b59379bb4ff0bc84650615a8938d71e87d9ae71
Jerico Navarro <jericoxnavarro16@gmail.com>
2020-12-11 21:49:52 +0800

    added controllers to make the codes readable

--- server/routes/Brands.routes.js
+++ server/routes/Brands.routes.js
@@ -1,87 +1,21 @@
 const express = require("express");
 const router = express.Router();
-const Brands = require("../models/Brands.model");
+
+const BrandsController = require("../controllers/Brands.controllers");
 
 // Get all Brands
-router.get("/brands", async (req, res) => {
-  try {
-    const brand = await Brands.find();
-    res.json(brand);
-  } catch (err) {
-    res.json({ message: err });
-  }
-});
+router.get("/brands", BrandsController.getBrands);
 
 // Get Brand
-router.get("/brands/:brandName", async (req, res) => {
-  try {
-    const brand = await Brands.find({ brandName: req.params.brandName });
-    const page = parseInt(req.query.page);
-    const limit = parseInt(req.query.limit);
-
-    const startIndex = (page - 1) * limit;
-    const endIndex = page * limit;
-    const pageLength = Math.ceil(brand[0].sneakers.length / limit);
-    const resultBrands = brand[0].sneakers.slice(startIndex, endIndex);
-    if (page > pageLength) {
-      res.json({
-        message: `Sorry, the page you're looking for doesn't exist.`,
-        data: false,
-      });
-    } else if (page < 1) {
-      res.json({
-        message: `Sorry, the page you're looking for doesn't exist.`,
-        data: false,
-      });
-    } else {
-      res.json({
-        pageLength: pageLength,
-        next: page + 1,
-        previous: page - 1,
-        limit: limit,
-        data: resultBrands,
-      });
-    }
-  } catch (err) {
-    res.json({ message: err });
-  }
-});
+router.get("/brands/:brandName", BrandsController.getBrand);
 
 // Add Brand
-router.post("/brands/add", async (req, res) => {
-  const brand = new Brands({
-    brandName: req.body.brandName,
-    sneakers: req.body.sneakers,
-  });
-  try {
-    const saveBrand = await brand.save();
-    res.json(saveBrand);
-  } catch (err) {
-    res.json({ message: err });
-  }
-});
+router.post("/brands/add", BrandsController.addBrand);
 
 // Update Brands
-router.patch("/brands/update/:brandId", async (req, res) => {
-  try {
-    const updateBrand = await Brands.updateOne(
-      { _id: req.params.brandId },
-      { $set: { sneakers: req.body.sneakers } }
-    );
-    res.json(updateBrand);
-  } catch (err) {
-    res.json({ message: err });
-  }
-});
+router.patch("/brands/update/:brandId", BrandsController.updateBrand);
 
 // Delete Brand
-router.delete("/brands/delete/:brandId", async (req, res) => {
-  try {
-    const removeBrand = await Brands.remove({ _id: req.params.brandId });
-    res.json(removeBrand);
-  } catch (err) {
-    res.json({ message: err });
-  }
-});
+router.delete("/brands/delete/:brandId", BrandsController.deleteBrand);
 
 module.exports = router;
