4c8794afc23144bfec530c79f823fb7f2737eae6
Jerico Navarro <jericoxnavarro16@gmail.com>
2020-12-13 23:03:28 +0800

    fix some bugs

--- client/src/components/Navbar/User.jsx
+++ client/src/components/Navbar/User.jsx
@@ -1,9 +1,16 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import Cookies from "universal-cookie";
+const cookies = new Cookies();
 
 const Guest = ({ name }) => {
   const [profile, setProfile] = useState(0);
   const [visibility, setVisibility] = useState("none");
+
+  const removeCookies = () => {
+    cookies.remove("_id");
+    cookies.remove("token");
+  };
 
   return (
     <>
@@ -15,11 +22,7 @@ const Guest = ({ name }) => {
         className="profile-nav"
       >
         <div className="profile-pic">
-          <img
-            src="https://scontent.fmnl4-4.fna.fbcdn.net/v/t1.0-9/79162681_3070225769722787_8615311949062389221_n.jpg?_nc_cat=102&ccb=2&_nc_sid=09cbfe&_nc_eui2=AeGlZTKbG5yjzguMFTihdM9iE0yb-x_Ch6cTTJv7H8KHp22Fd0auo0Rzpv8vhIRbgDSlEP9VQUWc2oWn3nPZYf2w&_nc_ohc=tqrol7dxxYUAX-0saUo&_nc_ht=scontent.fmnl4-4.fna&oh=e79ee6b310328496a52d37e6222566f5&oe=5FD2F758"
-            className="profilepic"
-            alt="Profile-Pic"
-          />
+          <img src="" className="profilepic" alt="Profile-Pic" />
         </div>
         <p className="user-fname">{name}</p>
       </div>
@@ -50,7 +53,7 @@ const Guest = ({ name }) => {
               </Link>
             </li>
             <li className="nav-item-profile">
-              <Link className="link-profile" to="/">
+              <Link onClick={removeCookies} className="link-profile" to="/">
                 <i className="fad fa-sign-out-alt"></i>Sign Out
               </Link>
             </li>
