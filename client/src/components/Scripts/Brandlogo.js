2477c1e43ea2a15fe59768a621671b7807b3c47f
Jerico Navarro <jericoxnavarro16@gmail.com>
2020-12-14 23:12:26 +0800

    clean some codes

--- client/src/components/Scripts/Brandlogo.js
+++ client/src/components/Scripts/Brandlogo.js
@@ -1,17 +1,17 @@
 const Brandlogo = (brand) => {
   let brandlogo;
   if (brand === "Nike" || brand === "Jordan") {
-    brandlogo = "./icons/svg/nike.svg";
+    brandlogo = "../icons/svg/nike.svg";
   } else if (brand === "adidas") {
-    brandlogo = "./icons/svg/adidas.svg";
+    brandlogo = "../icons/svg/adidas.svg";
   } else if (brand === "Vans") {
-    brandlogo = "./icons/svg/vans.svg";
+    brandlogo = "../icons/svg/vans.svg";
   } else if (brand === "Converse") {
-    brandlogo = "./icons/svg/converse.svg";
+    brandlogo = "../icons/svg/converse.svg";
   } else if (brand === "New Balance") {
-    brandlogo = "./icons/svg/newbalance.svg";
+    brandlogo = "../icons/svg/newbalance.svg";
   } else if (brand === "Reebok") {
-    brandlogo = "./icons/svg/reebok.svg";
+    brandlogo = "../icons/svg/reebok.svg";
   }
 
   return brandlogo;
