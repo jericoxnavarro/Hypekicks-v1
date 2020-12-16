4cdf4f358a4214822e51286178d3eaeb508cf784
Jerico Navarro <jericoxnavarro16@gmail.com>
2020-12-16 23:12:50 +0800

    validation is done

--- client/src/components/User/Signin.jsx
+++ client/src/components/User/Signin.jsx
@@ -1,16 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../../sass/Signin.scss";
 import Cookies from "universal-cookie";
 import { UserContext } from "../User.context";
 
 const Signin = () => {
   const cookies = new Cookies();
-  const [showstatus, setStatus] = React.useState("");
   const { _uid, token } = React.useContext(UserContext);
+  const [status, setStatus] = React.useState("");
+  const [onload, setOnload] = React.useState("");
   const [userid, setUserid] = _uid;
   const [usertoken, setUsertoken] = token;
 
   const submit = (e) => {
+    setOnload("1");
     e.preventDefault();
     let formData = [];
 
@@ -18,6 +21,26 @@ const Signin = () => {
     data.forEach(function (value, key) {
       formData.push(value);
     });
+
+    if (formData[0] === "") {
+      setOnload("");
+      return setStatus("Please Enter your username or email.");
+    } else if (formData[0].length < 6) {
+      setOnload("");
+      return setStatus("Username must be 6 characters or more.");
+    } else {
+      setStatus("");
+    }
+
+    if (formData[1] === "") {
+      setOnload("");
+      return setStatus("Please Enter your password.");
+    } else if (formData[1].length < 8) {
+      setOnload("");
+      return setStatus("Password must be 8 characters or more.");
+    } else {
+      setStatus("");
+    }
 
     fetch("http://localhost:3001/api/user/login", {
       method: "POST",
@@ -29,10 +52,11 @@ const Signin = () => {
     })
       .then((res) => res.json())
       .then((json) => {
-        console.log(json.status);
         if (json.status === "none") {
-          setStatus(json.message);
-          console.log(json.message);
+          setStatus(
+            "We couldn’t find an account matching the username and password you entered. Please check your username and password and try again."
+          );
+          setOnload("");
         } else {
           const date = new Date(Date.now() + 55920000);
           setUserid(json._id);
@@ -44,25 +68,57 @@ const Signin = () => {
             expires: date,
           });
           setStatus("");
+          setOnload("");
         }
       });
   };
 
+  const Status = () => {
+    if (status !== "") {
+      return <div className="status-message">{status}</div>;
+    } else {
+      return <></>;
+    }
+  };
+
+  const Onload = () => {
+    if (onload !== "") {
+      return (
+        <div className="loading">
+          <i className="fad fa-spinner-third"></i>
+        </div>
+      );
+    } else {
+      return (
+        <button type="submit" name="submit" className="submit">
+          Sign in
+        </button>
+      );
+    }
+  };
+
   return (
     <main className="main-signin">
+      <Status />
       <div className="hypekicks-info">
         <div className="container">
-          <h1 className="hype-name">
+          <Link to="/" className="hype-name">
             <span>HYPE</span>KICKS
-          </h1>
+          </Link>
           <p className="about-hypekicks">
             Discover the world’s top Sneakers & Prices.
           </p>
         </div>
       </div>
       <div className="hypekicks-signin">
+        <div className="sign-up">
+          <p className="not-member">Not a member?</p>
+          <Link to="/sign-up" className="sign-up-now">
+            Sign up now
+          </Link>
+        </div>
         <div className="container">
-          <form className="signin-form" onSubmit={submit}>
+          <form noValidate className="signin-form" onSubmit={submit}>
             <h1 className="signin-bigtext">Sign in to Hypekicks</h1>
             <label className="email-label">
               Username
@@ -72,14 +128,8 @@ const Signin = () => {
               Password
               <input required name="password" type="password" />
             </label>
-            <p className="error">
-              {showstatus.charAt(1).toUpperCase() +
-                showstatus.replaceAll('"', "").slice(1)}
-            </p>
             <div className="btn-wrapper">
-              <button type="submit" name="submit" className="submit">
-                Sign in
-              </button>
+              <Onload />
             </div>
           </form>
         </div>
