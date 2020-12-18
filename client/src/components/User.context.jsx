0af5a9cc316154f700811ffdbc3942944c1e6dd0
Jerico Navarro <jericoxnavarro16@gmail.com>
2020-12-18 22:55:17 +0800

    fetch user

--- client/src/components/User.context.jsx
+++ client/src/components/User.context.jsx
@@ -1,4 +1,4 @@
-import React, { useState, createContext } from "react";
+import React, { useState, createContext, useEffect } from "react";
 import Cookies from "universal-cookie";
 const cookies = new Cookies();
 
@@ -12,12 +12,40 @@ export const UserProvider = (props) => {
       return "";
     }
   };
+
   const [userid, setUserid] = useState(getCookie(cookies.get("_id")));
   const [usertoken, setUsertoken] = useState(getCookie(cookies.get("token")));
+  const [logged_In, setLogged_In] = useState(
+    getCookie(cookies.get("logged_in"))
+  );
+  const [user, setUser] = useState("");
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/api/user/${userid}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token": usertoken,
+      },
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        if (json) {
+          setUser(json);
+        } else {
+          setUser("");
+        }
+      });
+  }, [userid, usertoken]);
 
   return (
     <UserContext.Provider
-      value={{ _uid: [userid, setUserid], token: [usertoken, setUsertoken] }}
+      value={{
+        _uid: [userid, setUserid],
+        token: [usertoken, setUsertoken],
+        logged_in: [logged_In, setLogged_In],
+        _User: [user, setUser],
+      }}
     >
       {props.children}
     </UserContext.Provider>
